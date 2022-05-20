179e0b717176470912afe12b6b0e329d456408e1
chenchgf <chenchgf@yonyou.com>
2022-05-21 02:33:05 +0800

    hello spring rest

---
+++ hello-springrest/src/main/java/com/ccg/hellospringrest/HelloSpringrestApplication.java
@@ -0,0 +1,16 @@
+package com.ccg.hellospringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ref: [Getting Started | Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+ */
+@SpringBootApplication
+public class HelloSpringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringrestApplication.class, args);
+	}
+
+}
---
+++ hello-springrest/src/test/java/com/ccg/hellospringrest/HelloSpringrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccg.hellospringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
