38773889824a940786811671bc7e03dcfa41153b
chenchgf <chenchgf@yonyou.com>
2022-05-25 02:04:52 +0800

    hellp spring cloud hystrix

---
+++ hello-springcloud/micro-service-cloud-consumer-dept-feign/src/main/java/com/ccg/service/DeptHystrixFallBackService.java
@@ -0,0 +1,20 @@
+package com.ccg.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Hystrix 服务降级
+ * 解耦回退逻辑
+ */
+@Component
+public class DeptHystrixFallBackService implements DeptHystrixService {
+	@Override
+	public String deptInfo_Ok(Integer id) {
+		return "--------------------系统繁忙，请稍后重试！（解耦回退方法触发）-----------------------";
+	}
+
+	@Override
+	public String deptInfo_Timeout(Integer id) {
+		return "--------------------系统繁忙，请稍后重试！（解耦回退方法触发）-----------------------";
+	}
+}
---
+++ hello-springcloud/micro-service-cloud-consumer-dept-hystrix-dashboard-9002/src/main/java/com/ccg/MicroServiceCloudConsumerDeptHystrixDashboard9002Application.java
@@ -0,0 +1,13 @@
+package com.ccg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MicroServiceCloudConsumerDeptHystrixDashboard9002Application {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCloudConsumerDeptHystrixDashboard9002Application.class, args);
+	}
+}
